9184a4d8b200239861e6a42305cffb1ea49539ab
Andreas Eismann <a.eismann@senbax.de>
2025-04-13 15:29:06 +0200

    fix: Initial version.

---
+++ clock.js
@@ -0,0 +1,97 @@
+import m from "mithril";
+import tagl from "tagl-mithril";
+
+// prettier-ignore
+const { address, aside, footer, header, h1, h2, h3, h4, h5, h6, hgroup, main, nav, section, article, blockquote, dd, dir, div, dl, dt, figcaption, figure, hr, li, ol, p, pre, ul, a, abbr, b, bdi, bdo, br, cite, code, data, dfn, em, i, kdm, mark, q, rb, rp, rt, rtc, ruby, s, samp, small, span, strong, sub, sup, time, tt, u, wbr, area, audio, img, map, track, video, embed, iframe, noembed, object, param, picture, source, canvas, noscript, script, del, ins, caption, col, colgroup, table, tbody, td, tfoot, th, thead, tr, button, datalist, fieldset, form, formfield, input, label, legend, meter, optgroup, option, output, progress, select, textarea, details, dialog, menu, menuitem, summary, content, element, slot, template } = tagl(m);
+const { svg, circle, title, g, path, defs, use, style } = tagl(m);
+
+export default (vnode) => {
+  return {
+    view(vnode) {
+      const { hour, minute, second } = vnode.attrs;
+
+      return [
+        svg(
+          {
+            xmlns: "http://www.w3.org/2000/svg",
+            "xmlns:xlink": "http://www.w3.org/1999/xlink",
+            viewBox: "-1024 -1024 2048 2048",
+            width: "600",
+            height: "600",
+          },
+          [
+            title("Swiss Railway Clock"),
+            style(
+              { type: "text/css" },
+              " .bg {stroke: none; fill: white;} .fc {stroke: none; fill: black;} .h3 {stroke: none; fill: black;} .h2 {stroke: none; fill: #0000aa;} .h1 {stroke: none; fill: #aa0000;} "
+            ),
+            defs([
+              path({ id: "mark1", d: "M -20,-1000 l 40,0 0,100 -40,0 z" }),
+              path({ id: "mark2", d: "M -40,-1000 l 80,0 0,240 -80,0 z" }),
+              path({ id: "mark3", d: "M -40,-1000 l 80,0 0,300 -80,0 z" }),
+              path({
+                id: "handh",
+                d: "M -50,-600  l 50,-50 50,50 0,800  -100,0 z",
+              }),
+              path({
+                id: "handm",
+                d: "M -40,-900  l 40,-40 40,40 0,1180 -80,0  z",
+              }),
+              g({ id: "hands" }, [
+                path({
+                  d: "M -10,-910 l  10,-10 10,10 2,300 -24,0 z\n               M -13,-390 l  26,0         7,690 -40,0 z",
+                }),
+                path({
+                  d: "M   0,-620 a 120,120 0 0 1 0,240\n                          a 120,120 0 0 1 0,-240 z\n               M   0,-560 a  60,60  0 0 0 0,120\n                          a  60,60  0 0 0 0,-120 z",
+                }),
+              ]),
+              g({ id: "face1" }, [
+                use({ "xlink:href": "#mark1", transform: "rotate(06)" }),
+                use({ "xlink:href": "#mark1", transform: "rotate(12)" }),
+                use({ "xlink:href": "#mark1", transform: "rotate(18)" }),
+                use({ "xlink:href": "#mark1", transform: "rotate(24)" }),
+              ]),
+              g({ id: "face2" }, [
+                use({ "xlink:href": "#face1" }),
+                use({ "xlink:href": "#face1", transform: "rotate(30)" }),
+                use({ "xlink:href": "#face1", transform: "rotate(60)" }),
+                use({ "xlink:href": "#mark3" }),
+                use({ "xlink:href": "#mark2", transform: "rotate(30)" }),
+                use({ "xlink:href": "#mark2", transform: "rotate(60)" }),
+              ]),
+              g({ id: "face" }, [
+                use({ "xlink:href": "#face2" }),
+                use({ "xlink:href": "#face2", transform: "rotate(90)" }),
+                use({ "xlink:href": "#face2", transform: "rotate(180)" }),
+                use({ "xlink:href": "#face2", transform: "rotate(270)" }),
+              ]),
+            ]),
+            circle({ class: "bg", r: "1024" }),
+            use({ class: "fc", "xlink:href": "#face" }),
+            hour == null
+              ? null
+              : use({
+                  class: "h1",
+                  "xlink:href": "#handh",
+                  transform: `rotate(${(hour * 360) / 12})`,
+                }),
+            minute == null
+              ? null
+              : use({
+                  class: "h2",
+                  "xlink:href": "#handm",
+                  transform: `rotate(${(minute * 360) / 60})`,
+                }),
+            second == null
+              ? null
+              : use({
+                  class: "h3",
+                  "xlink:href": "#hands",
+                  transform: `rotate(${(second * 360) / 60})`,
+                }),
+          ]
+        ),
+      ];
+    },
+  };
+};
---
+++ main.js
@@ -0,0 +1,236 @@
+import m from "mithril";
+import tagl, { mo, time } from "tagl-mithril";
+import clock from "./clock";
+
+const { h1, div, p, input, button, select, option, span } = tagl(m);
+const { trunc } = Math;
+setInterval(m.redraw, 10);
+
+const state = {
+  hour: 0,
+  minute: 0,
+  second: null,
+  size: 120,
+  modus: "linear",
+  tz: "Europe/Berlin",
+};
+
+const resetTime = () => {
+  state.hour = null;
+  state.minute = null;
+  state.second = null;
+  m.redraw();
+};
+
+const nav = (vnode) => ({
+  view: (vnode) => {
+    return div.container(
+      button({ onclick: () => m.route.set("/") }, "Uhr"),
+      button({ onclick: () => m.route.set("/clock") }, "Spielen")
+    );
+  },
+});
+
+const modi = {
+  linear: (h, m, s) => {
+    return {
+      hour: h,
+      minute: m,
+      second: s,
+    };
+  },
+  clack: (h, m, s) => {
+    return {
+      hour: trunc(h),
+      minute: trunc(m),
+      second: trunc(s),
+    };
+  },
+  Sekunden: (h, m, s) => {
+    return {
+      hour: h,
+      minute: m,
+      second: trunc(s),
+    };
+  },
+};
+
+const simpleClock = (vnode) => ({
+  view: (vnode) => {
+    const time_local = new Date();
+
+    const time = new Date(
+      time_local.toLocaleString("en-US", {
+        timeZone: state.tz,
+      })
+    );
+    const hor =
+      time.getHours() + time.getMinutes() / 60 + time.getSeconds() / 3600;
+    const min =
+      time.getMinutes() +
+      time.getSeconds() / 60 +
+      time.getMilliseconds() / 60000;
+    const sec = time.getSeconds() + time.getMilliseconds() / 1000;
+
+    const { hour, minute, second } = modi[state.modus](hor, min, sec);
+
+    return div.container(
+      h1("Lerne die Uhrzeit"),
+      m(nav),
+      select(
+        {
+          onchange: (e) => {
+            state.modus = e.target.value;
+            m.redraw();
+          },
+        },
+        Object.keys(modi).map((key) => option({ value: key }, key))
+      ),
+      select(
+        { value: state.tz, onchange: (e) => (state.tz = e.target.value) },
+        Intl.supportedValuesOf("timeZone").map((key) =>
+          option({ value: key }, key)
+        )
+      ),
+      h1(
+        `${trunc(hor).toString().padStart(2, "0")}:${trunc(min)
+          .toString()
+          .padStart(2, "0")}:${trunc(sec).toString().padStart(2, "0")}`
+      ),
+      m(clock, { hour, minute, second, size: 120 })
+    );
+  },
+});
+
+const moveHour = (increment) => {
+  state.hour = (state.hour + increment + 24) % 24;
+};
+
+const moveMinute = (increment) => {
+  state.minute = (state.minute + increment + 60) % 60;
+  if (state.minute === 0 && increment > 0) {
+    moveHour(1);
+  }
+  if (state.minute === 59 && increment < 0) {
+    moveHour(-1);
+  }
+};
+
+const moveSecond = (increment) => {
+  state.second = (state.second + increment + 60) % 60;
+  if (state.second === 0 && increment > 0) {
+    moveMinute(1);
+  }
+  if (state.second === 59 && increment < 0) {
+    moveMinute(-1);
+  }
+};
+const hour_keydown = (e) => {
+  if (e.key === "ArrowUp") {
+    moveHour(1);
+  }
+  if (e.key === "ArrowDown") {
+    moveHour(-1);
+  }
+  m.redraw();
+  e.preventDefault();
+};
+const minute_keydown = (e) => {
+  if (e.key === "ArrowUp") {
+    moveMinute(1);
+  }
+  if (e.key === "ArrowDown") {
+    moveMinute(-1);
+  }
+  m.redraw();
+  e.preventDefault();
+};
+const second_keydown = (e) => {
+  if (e.key === "ArrowUp") {
+    moveSecond(1);
+  }
+  if (e.key === "ArrowDown") {
+    moveSecond(-1);
+  }
+  m.redraw();
+  e.preventDefault();
+};
+
+const clockComponent = (vnode) => ({
+  view: (vnode) => {
+    const time = new Date();
+    //   console.log(time, state);
+    let second =
+      state.second === null
+        ? time.getSeconds() + time.getMilliseconds() / 1000
+        : state.second;
+    second =
+      (state.hour != null || state.minute != null) && state.second == null
+        ? null
+        : second;
+    const minute =
+      (state.minute === null ? time.getMinutes() : state.minute) +
+      (second % 60) / 60;
+    const hour =
+      (state.hour === null ? time.getHours() : state.hour) + (minute % 60) / 60;
+    return div.container(
+      m(nav),
+      button({ onclick: () => (state.hour = (state.hour + 12) % 24) }, "+"),
+      input({
+        type: "number",
+        value: state.hour,
+        onkeydown: hour_keydown,
+        oninput: (e) => (state.hour = (+e.target.value + 24) % 24),
+      }),
+      input({
+        type: "number",
+        value: state.minute,
+        onkeydown: minute_keydown,
+        oninput: (e) =>
+          (state.minute =
+            e.target.value !== "" ? (+e.target.value + 60) % 60 : null),
+      }),
+      input({
+        type: "number",
+        value: state.second,
+        onkeydown: second_keydown,
+        oninput: (e) =>
+          (state.second =
+            e.target.value !== "" ? (+e.target.value + 60) % 60 : null),
+      }),
+      button({ onclick: () => resetTime() }, "×"),
+      h1(
+        span(
+          { style: "color:#cc0000" },
+          `${trunc((hour + 12) % 24)
+            .toString()
+            .padStart(2, "0")}`
+        ),
+        span(":"),
+        span(
+          { style: "color:#0000bb" },
+          `${trunc(minute).toString().padStart(2, "0")}`
+        )
+      ),
+
+      h1(
+        span(
+          { style: "color:#cc0000" },
+          `${trunc(hour).toString().padStart(2, "0")}`
+        ),
+        span(":"),
+        span(
+          { style: "color:#0000bb" },
+          `${trunc(minute).toString().padStart(2, "0")}`
+        )
+      ),
+      // p('With the help of tagl'),
+      m(clock, { hour, minute, second, size: state.size })
+    );
+  },
+});
+
+m.route(document.body, "/", {
+  "/": simpleClock,
+  "/clock": clockComponent,
+});
